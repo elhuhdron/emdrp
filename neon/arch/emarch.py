25dbf9973c69d6848292c28e7a38918307c0fa98
Paul Watkins <pwatkins@gmail.com>
2016-05-12 18:21:47 -0400

    more tweaking

--- neon/arch/emarch.py
+++ neon/arch/emarch.py
@@ -22,7 +22,7 @@
 
 from neon.initializers import Constant, Gaussian, Uniform
 from neon.layers import Conv, Dropout, Pooling, Affine, LRN
-from neon.transforms import Rectlin, Logistic, Softmax # Identity
+from neon.transforms import Rectlin, Logistic, Softmax, Identity
 
 class EMModelArchitecture(object):
     def __init__(self, noutputs, use_softmax):
@@ -49,20 +49,20 @@ class fergus(EMModelArchitecture):
                  padding=3, strides=1),
             LRN(31, ascale=0.001, bpower=0.75),
             Pooling(3, strides=2),
-            Conv((5, 5, 256), init=Gaussian(scale=0.01), bias=Constant(1), activation=Rectlin(), 
+            Conv((5, 5, 256), init=Gaussian(scale=0.01), bias=Constant(0), activation=Rectlin(), 
                  padding=2, strides=1),
             LRN(31, ascale=0.001, bpower=0.75),
             Pooling(3, strides=2),
             Conv((3, 3, 384), init=Gaussian(scale=0.03), bias=Constant(0), activation=Rectlin(), 
                  padding=1, strides=1),
-            Conv((3, 3, 384), init=Gaussian(scale=0.03), bias=Constant(1), activation=Rectlin(), 
+            Conv((3, 3, 384), init=Gaussian(scale=0.03), bias=Constant(0), activation=Rectlin(), 
                  padding=1, strides=1),
-            Conv((3, 3, 256), init=Gaussian(scale=0.03), bias=Constant(1), activation=Rectlin(), 
+            Conv((3, 3, 256), init=Gaussian(scale=0.03), bias=Constant(0), activation=Rectlin(), 
                  padding=1, strides=1),
             Pooling(3, strides=2),
-            Affine(nout=4096, init=Gaussian(scale=0.01), bias=Constant(0), activation=Rectlin()),
+            Affine(nout=4096, init=Gaussian(scale=0.01), bias=Constant(0), activation=Identity()),
             Dropout(keep=0.5),
-            Affine(nout=4096, init=Gaussian(scale=0.01), bias=Constant(0), activation=Rectlin()),
+            Affine(nout=4096, init=Gaussian(scale=0.01), bias=Constant(0), activation=Identity()),
             Dropout(keep=0.5),
             Affine(nout=self.noutputs, init=Gaussian(scale=0.01), bias=Constant(0), 
                    activation=Softmax() if self.use_softmax else Logistic(shortcut=True))
