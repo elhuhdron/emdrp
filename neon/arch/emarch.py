42db2af36732bb5348ac1dd0dbb68c51d478652b
Paul Watkins <pwatkins@gmail.com>
2016-08-04 16:45:34 -0400

    vgg3pool mod for test

--- neon/arch/emarch.py
+++ neon/arch/emarch.py
@@ -305,7 +305,7 @@ class vgg3pool(EMModelArchitecture):
             Conv((3, 3, 384), init=Kaiming(), activation=Explin(), batch_norm=bn, padding=1, strides=1),
             Conv((3, 3, 384), init=Kaiming(), activation=Explin(), batch_norm=bn, padding=1, strides=1),
             Conv((3, 3, 384), init=Kaiming(), activation=Explin(), batch_norm=bn, padding=1, strides=1),
-            Conv((3, 3, 384), init=Kaiming(), activation=Explin(), batch_norm=bn, padding=1, strides=1),
+            #Conv((3, 3, 384), init=Kaiming(), activation=Explin(), batch_norm=bn, padding=1, strides=1),
             Pooling(3, strides=2, padding=1),
             # 8
             Conv((3, 3, 6144), init=Kaiming(), activation=Explin(), batch_norm=bn, padding=1, strides=1),
