e16aca6b95134401f8803633ef414dc25952f136
Paul Watkins <pwatkins@gmail.com>
2017-09-07 14:03:11 -0400

    run5 copies to knossos

---
+++ scripts/shell/K0057_D31_dsx3y3z1-run5/convert_to_knossos_mesh.sh
@@ -0,0 +1,23 @@
+
+# run on blue in ~/gits/emdrp/recon/python
+
+declare -a thrs=("0.99900000" "0.99925000" "0.99950000" "0.99975000")
+declare -a synin=("syn2" "syn" "syn" "syn2")
+level=0
+while [ $level -lt 4 ]
+do
+    echo processing ${thrs[$level]}
+
+    python -u dpCubeIter.py --volume_range_beg 2 8 1 --volume_range_end 50 38 19 --overlap 0 0 0 --cube_size 6 6 6 --cmd "cp" --fileflags 0 0 --filepaths /mnt/${synin[$level]}/watkinspv/full_datasets/neon/mfergus32_K0057_ds3_run5/mesh_wtsh /mnt/ext/K0057_D31/cubes_dsx3y3z1/mag1 --fileprefixes K0057_D31_mag1 K0057_D31_mag1 --filepostfixes .clean_wtshA.$level.mesh.h5 .clean_wtshB.$level.mesh.h5 --filepaths-affixes 0 1 --no_volume_flags > tmp_out${level}_1.sh
+
+    # echo current copy to output file
+    python -u dpCubeIter.py --volume_range_beg 2 8 1 --volume_range_end 50 38 19 --overlap 8 8 8 --cube_size 6 6 6 --cmd "echo " --fileflags 0 --filepaths 'copying to ' --fileprefixes '' --filepostfixes ' ' --filepaths-affixes 0 --no_volume_flags > tmp_out${level}_0.sh
+
+    # put into single command, ; delimited
+    paste -d';' tmp_out${level}_0.sh tmp_out${level}_1.sh > tmp_out${level}.sh
+
+    #nohup sh tmp_out${level}.sh >& tmp_K0057_D31_dsx3y3z1-run5-cp-mesh${level}.txt &
+
+    level=`expr $level + 1`
+done
+
---
+++ scripts/shell/K0057_D31_dsx3y3z1-run5/convert_to_knossos_seg.sh
@@ -0,0 +1,16 @@
+
+# run on blue in ~/gits/emdrp/recon/python
+
+declare -a thrs=("0.99900000" "0.99925000" "0.99950000" "0.99975000")
+level=0
+while [ $level -lt 4 ]
+do
+    echo processing ${thrs[$level]}
+
+    python -u dpCubeIter.py --volume_range_beg 2 8 1 --volume_range_end 50 38 19 --overlap 0 0 0 --cube_size 1 1 1 --cmd "python -u $HOME/gits/emdrp/recon/python/dpLoadh5.py --dataset labels --subgroups with_background ${thrs[$level]} --data-type uint64 --raw-compression --dpL" --fileflags outraw srcfile --filepaths "/mnt/ext/K0057_D31/cubes_dsx3y3z1/mag1" /Data_yello/watkinspv/full_datasets/neon/mfergus32_K0057_ds3_run5/clean_wtsh --fileprefixes K0057_D31_mag1 K0057_D31_dsx3y3z1_supervoxels_clean --filepostfixes .clean_wtshB.$level.seg .h5 --filepaths-affixes 1 0 --filemodulators 1 1 1  6 6 6 > tmp_out${level}.sh
+
+    nohup sh tmp_out${level}.sh >& tmp_K0057_D31_dsx3y3z1-run5-cp-seg${level}.txt &
+
+    level=`expr $level + 1`
+done
+
