a3da0e23819de7310861e00f4d64054ab500552e
Paul Watkins <pwatkins@gmail.com>
2017-09-09 18:10:50 -0400

    typo

--- scripts/shell/K0057_D31_dsx3y3z1-run5/split_transfer.sh
+++ scripts/shell/K0057_D31_dsx3y3z1-run5/split_transfer.sh
@@ -1,8 +1,7 @@
 
 # run on green,red,infra,ultra
 # modify appropriately
-# next time modify this to split into 3 groups on each nas
-#   then use two rsyncs on the last machine to copy from the 3rd group on each
+# NOTE: next time just stick to 4 or 5 transfers, multiple xfers on one machine slows the overall transfer rate
 
 d=`pwd`
 cd /mnt/syn2/watkinspv/full_datasets/neon/mfergus32_K0057_ds3_run5/probs
@@ -11,7 +10,7 @@ split -l 20 files.txt tmp.
 mkdir 01 02 03
 xargs -a tmp.aa mv -t 01
 xargs -a tmp.ab mv -t 02
-xargs -a tmp.ab mv -t 02
+xargs -a tmp.ac mv -t 02
 rm files.txt tmp.a?
 cd /mnt/syn/watkinspv/full_datasets/neon/mfergus32_K0057_ds3_run5/probs
 find . -name '*.h5' > files.txt
@@ -19,7 +18,7 @@ split -l 20 files.txt tmp.
 mkdir 04 05 06
 xargs -a tmp.aa mv -t 04
 xargs -a tmp.ab mv -t 05
-xargs -a tmp.ab mv -t 06
+xargs -a tmp.ac mv -t 06
 rm files.txt tmp.a?
 cd $d
 
