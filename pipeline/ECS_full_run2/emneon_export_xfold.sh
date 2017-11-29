967afb4adf2c4dfbdddc3ee28a8efb8b2bb80276
Paul Watkins <pwatkins@gmail.com>
2017-11-29 17:41:52 -0500

    fix

--- pipeline/ECS_full_run2/emneon_export_xfold.sh
+++ pipeline/ECS_full_run2/emneon_export_xfold.sh
@@ -4,6 +4,7 @@
 
 declare -a card=("0" "1" "2" "3" "0" "1" "2" "3" "0" "1" "2" "3" "0" "1" "2" "3" "0" "1" "2" "3")
 declare -a skip_lists=("0" "1" "2" "3" "4" "5")
+declare -a slices=('xyz' 'xyz' 'xyz' 'xyz')
 
 # run for both datasets
 dataset=M0007
@@ -18,9 +19,9 @@ do
 
     icount=0
     count=$icount
-    while [ $count -lt 4 ]
+    while [ $count -lt ${#slices[@]} ]
     do
-        echo "python -u ./emneon.py --data_config ~/gits/emdrp/pipeline/ECS_full_run2/EMdata-3class-64x64out-export-${dataset}.ini --model_file /mnt/syn2/watkinspv/convnet_out/neon_xfold/vgg3pool64_ECS_full_run2/${dataset}_xyz_test${itestc}_${count}.prm --write_output /mnt/syn2/watkinspv/full_datasets/neon_xfold/vgg3pool64_ECS_full_run2/${dataset}_xyz_${count}.h5 --test_range 200001 200256 --nbebuf 1 -i ${card[$totalc]} --chunk_skip_list $testc" > run_tmp.sh
+        echo "python -u ./emneon.py --data_config ~/gits/emdrp/pipeline/ECS_full_run2/EMdata-3class-64x64out-export-${dataset}.ini --model_file /mnt/syn2/watkinspv/convnet_out/neon_xfold/vgg3pool64_ECS_full_run2/${dataset}_${slices[$count]}_test${itestc}_${count}.prm --write_output /mnt/syn2/watkinspv/full_datasets/neon_xfold/vgg3pool64_ECS_full_run2/${dataset}_${slices[$count]}_test${itestc}_${count}.h5 --test_range 200001 200256 --nbebuf 1 -i ${card[$totalc]} --chunk_skip_list $testc" > run_tmp.sh
         #cat run_tmp.sh
         ./run_gpu_job.py -s run_tmp.sh
        
