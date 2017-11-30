002a066d0f054aa06c6b56c99bf2fedbacb82e69
Paul Watkins <pwatkins@gmail.com>
2017-11-30 13:31:50 -0500

    typo

--- pipeline/ECS_full_run2/emneon_export.sh
+++ pipeline/ECS_full_run2/emneon_export.sh
@@ -17,7 +17,7 @@ icount=0
 count=$icount
 while [ $count -lt ${#slices[@]} ]
 do
-    echo "python -u ./emneon.py --data_config ~/gits/emdrp/pipeline/ECS_full_run2/EMdata-3class-64x64out-export-ctx-${dataset}.ini --model_file /mnt/syn2/watkinspv/convnet_out/neon/vgg3pool64_ECS_full_run2/${dataset}_${slices[$count]}_${count}.prm --write_output /mnt/syn2/watkinspv/full_datasets/neon/vgg3pool64_ECS_full_run2/${dataset}_${slices[$count]}_${count}.h5 --test_range 200001 200400 --nbebuf 1 -i ${card[$totalc]}" > run_tmp.sh
+    echo "python -u ./emneon.py --data_config ~/gits/emdrp/pipeline/ECS_full_run2/EMdata-3class-64x64out-export-ctx-${dataset}.ini --model_file /mnt/syn2/watkinspv/convnet_out/neon/vgg3pool64_ECS_full_run2/${dataset}_${slices[$count]}_${count}.prm --write_output /mnt/syn2/watkinspv/full_datasets/neon/vgg3pool64_ECS_full_run2/${dataset}_${slices[$count]}_${count}.h5 --test_range 200001 200${ctx_cubes} --nbebuf 1 -i ${card[$totalc]}" > run_tmp.sh
     #cat run_tmp.sh
     ./run_gpu_job.py -s run_tmp.sh
        
