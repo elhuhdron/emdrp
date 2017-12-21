c99e585ab5056bab9fc96c221c3488b3bf7bb6b8
Paul Watkins <pwatkins@gmail.com>
2017-12-21 15:12:56 -0500

    transfer probs / plot kernels

---
+++ pipeline/k0725_dsx2y2z1-run1/emneon_plot_kernels_eg.sh
@@ -0,0 +1,6 @@
+
+./emneon.py --model_file ~/Downloads/tmp_kern/k0725_dsx2y2z1_xyz_0.prm --plot_weight_layer 0 -i 0 --plot_norm_per_filter
+./emneon.py --model_file ~/Downloads/tmp_kern/k0725_dsx2y2z1_xyz_1.prm --plot_weight_layer 0 -i 0 --plot_norm_per_filter
+./emneon.py --model_file ~/Downloads/tmp_kern/k0725_dsx2y2z1_xzy_0.prm --plot_weight_layer 0 -i 0 --plot_norm_per_filter
+./emneon.py --model_file ~/Downloads/tmp_kern/k0725_dsx2y2z1_zyx_0.prm --plot_weight_layer 0 -i 0 --plot_norm_per_filter
+
---
+++ pipeline/k0725_dsx2y2z1-run1/split_transfer.sh
@@ -0,0 +1,23 @@
+
+# run on green,red,infra,ultra
+# modify appropriately
+
+d=`pwd`
+cd /mnt/syn/watkinspv/full_datasets/neon/vgg3pool64_k0725_ds2_run1/probs
+find . -name '*.h5' > files.txt
+split -l 77 files.txt tmp.
+mkdir 01 02
+xargs -a tmp.aa mv -t 01
+xargs -a tmp.ab mv -t 02
+rm files.txt tmp.a?
+cd /mnt/syn2/watkinspv/full_datasets/neon/vgg3pool64_k0725_ds2_run1/probs
+find . -name '*.h5' > files.txt
+split -l 108 files.txt tmp.
+mkdir 03 04
+xargs -a tmp.aa mv -t 03
+xargs -a tmp.ab mv -t 04
+rm files.txt tmp.a?
+cd $d
+
+#rsync -avh --progress -e ssh /mnt/syn2/watkinspv/full_datasets/neon/mfergus32_K0057_ds3_run5/probs/04 watkinspv@biowulf.nih.gov:/data/CDCU/full_datasets/neon/mfergus32_K0057_ds3_run5/probs/
+
