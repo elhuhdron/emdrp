4f424b3420a882e3a7ccb57c0ea3ee2c595dba02
Paul Watkins <pwatkins@gmail.com>
2017-12-21 16:09:30 -0500

    fix

--- pipeline/k0725_dsx2y2z1-run1/split_transfer.sh
+++ pipeline/k0725_dsx2y2z1-run1/split_transfer.sh
@@ -5,19 +5,20 @@
 d=`pwd`
 cd /mnt/syn/watkinspv/full_datasets/neon/vgg3pool64_k0725_ds2_run1/probs
 find . -name '*.h5' > files.txt
-split -l 77 files.txt tmp.
+split -l 72 files.txt tmp.
 mkdir 01 02
 xargs -a tmp.aa mv -t 01
 xargs -a tmp.ab mv -t 02
 rm files.txt tmp.a?
 cd /mnt/syn2/watkinspv/full_datasets/neon/vgg3pool64_k0725_ds2_run1/probs
 find . -name '*.h5' > files.txt
-split -l 108 files.txt tmp.
-mkdir 03 04
+split -l 72 files.txt tmp.
+mkdir 03 04 05
 xargs -a tmp.aa mv -t 03
 xargs -a tmp.ab mv -t 04
+xargs -a tmp.ac mv -t 05
 rm files.txt tmp.a?
 cd $d
 
-#rsync -avh --progress -e ssh /mnt/syn2/watkinspv/full_datasets/neon/mfergus32_K0057_ds3_run5/probs/04 watkinspv@biowulf.nih.gov:/data/CDCU/full_datasets/neon/mfergus32_K0057_ds3_run5/probs/
+#rsync -avh --progress -e ssh /mnt/syn/watkinspv/full_datasets/neon/vgg3pool64_k0725_ds2_run1/probs/01 watkinspv@biowulf.nih.gov:/data/CDCU/full_datasets/neon/vgg3pool64_k0725_ds2_run1/probs/
 
