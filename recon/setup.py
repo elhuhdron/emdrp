d2625857bf769246c70daddce38513f2d097779c
Eric Jelli <eric.jelli@caesar.de>
2021-04-23 13:30:41 +0200

    add setup.py and __init__.py to make emdrp pip installable

---
+++ recon/setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup
+
+setup (name = 'emdrp',
+       version = '0.1',
+       author = 'Paul Watkins',
+       author_email = 'pwatkins@gmail.com',
+       url = 'https://github.com/elhuhdron/emdrp',
+       license='MIT',
+       install_requires=[
+          'numpy',
+      ],
+       )
